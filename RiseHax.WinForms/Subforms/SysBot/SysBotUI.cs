03695226c14c0a1c6f02a5990631bbc229396bc9
Glazelf <gideon.steenbergen@gmail.com>
2021-01-19 23:40:03 +0100

    create socket connection
    also flip fields accordingly

--- RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
+++ RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
@@ -1,23 +1,52 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace RiseHax.WinForms
 {
     public partial class SysBotUI : Form
     {
+        Socket socket;
         public SysBotUI()
         {
             InitializeComponent();
         }
 
-        private void SysBotUI_Load(object sender, EventArgs e)
-        {
+        public bool Connected = false;
 
+        public void SysBotUI_Load(object sender, EventArgs e)
+        {
         }
 
         private void ButtonConnect_Click(object sender, EventArgs e)
         {
+            int Port = int.Parse(TextBoxPort.Text);
+            if (Connected == true)
+            {
+                Connect(TextBoxIP.Text, Port, true);
+                TextBoxIP.Enabled = true;
+                TextBoxPort.Enabled = true;
 
+                QuestSysBotMonsterHPCount.Enabled = false;
+                QuestSysBotPlayerHPCount.Enabled = false;
+                QuestSysBotPouchMegaPotionCount.Enabled = false;
+                ButtonSysbotQuestRead.Enabled = false;
+
+                ButtonConnect.Text = "Connect";
+            }
+            else
+            {
+                Connect(TextBoxIP.Text, Port, false);
+                TextBoxIP.Enabled = false;
+                TextBoxPort.Enabled = false;
+
+                QuestSysBotMonsterHPCount.Enabled = true;
+                QuestSysBotPlayerHPCount.Enabled = true;
+                QuestSysBotPouchMegaPotionCount.Enabled = true;
+                ButtonSysbotQuestRead.Enabled = true;
+
+                ButtonConnect.Text = "Diconnect";
+            }
         }
 
         private void QuestSysBotMonsterHPCount_ValueChanged(object sender, EventArgs e)
@@ -38,6 +67,32 @@ namespace RiseHax.WinForms
         private void ButtonSysbotQuestRead_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void Connect(string host, int port, bool disconnect = false)
+        {
+            if (disconnect == false)
+            {
+                socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+                socket.Connect(host, port);
+                MessageBox.Show("Connection established!",
+                    "RiseHax Connection",
+                    MessageBoxButtons.OK
+                );
+                Connected = true;
+            }
+            else
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                MessageBox.Show("Connection aborted!",
+                "RiseHax Connection",
+                MessageBoxButtons.OK
+            );
+                Connected = false;
+            }
         }
     }
 }
