cce22983c249ebcd8ae8e4c17b0f98b0472f7b70
Glazelf <gideon.steenbergen@gmail.com>
2021-01-19 22:05:51 +0100

    Positioning

--- RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
+++ RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
@@ -15,32 +15,7 @@ namespace RiseHax.WinForms
 
         }
 
-        private void TextBoxIP_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void ButtonConnect_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void GroupConnection_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void LabelIP_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void LabelPort_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBoxPort_TextChanged(object sender, EventArgs e)
         {
 
         }
