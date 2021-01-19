a59ddc6c9f88ede6a2fb707be46d0459158a0caa
Glazelf <gideon.steenbergen@gmail.com>
2021-01-19 20:02:11 +0100

    Add connection options

--- RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
+++ RiseHax.WinForms/Subforms/SysBot/SysBotUI.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RiseHax.WinForms
@@ -18,6 +11,36 @@ namespace RiseHax.WinForms
         }
 
         private void SysBotUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupConnection_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelIP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelPort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxPort_TextChanged(object sender, EventArgs e)
         {
 
         }
