29f93f6013ddbdac252816fd6ba0efc432c31c94
Glazelf <gideon.steenbergen@gmail.com>
2021-01-19 20:02:11 +0100

    Add connection options

--- RiseHax.WinForms/Subforms/SysBot/SysBotUI.Designer.cs
+++ RiseHax.WinForms/Subforms/SysBot/SysBotUI.Designer.cs
@@ -29,21 +29,107 @@ namespace RiseHax.WinForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.TextBoxIP = new System.Windows.Forms.TextBox();
+            this.ButtonConnect = new System.Windows.Forms.Button();
+            this.GroupConnection = new System.Windows.Forms.GroupBox();
+            this.TextBoxPort = new System.Windows.Forms.TextBox();
+            this.LabelIP = new System.Windows.Forms.Label();
+            this.LabelPort = new System.Windows.Forms.Label();
+            this.GroupConnection.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TextBoxIP
+            // 
+            this.TextBoxIP.AccessibleName = "TextBoxIP";
+            this.TextBoxIP.Location = new System.Drawing.Point(40, 28);
+            this.TextBoxIP.Name = "TextBoxIP";
+            this.TextBoxIP.Size = new System.Drawing.Size(87, 20);
+            this.TextBoxIP.TabIndex = 0;
+            this.TextBoxIP.Text = "192.168.0.1";
+            this.TextBoxIP.TextChanged += new System.EventHandler(this.TextBoxIP_TextChanged);
+            // 
+            // ButtonConnect
+            // 
+            this.ButtonConnect.AccessibleName = "ButtonConnect";
+            this.ButtonConnect.Location = new System.Drawing.Point(40, 81);
+            this.ButtonConnect.Name = "ButtonConnect";
+            this.ButtonConnect.Size = new System.Drawing.Size(71, 20);
+            this.ButtonConnect.TabIndex = 2;
+            this.ButtonConnect.Text = "Connect";
+            this.ButtonConnect.UseVisualStyleBackColor = true;
+            this.ButtonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // GroupConnection
+            // 
+            this.GroupConnection.AccessibleName = "GroupConnection";
+            this.GroupConnection.Controls.Add(this.LabelPort);
+            this.GroupConnection.Controls.Add(this.LabelIP);
+            this.GroupConnection.Controls.Add(this.TextBoxPort);
+            this.GroupConnection.Controls.Add(this.ButtonConnect);
+            this.GroupConnection.Controls.Add(this.TextBoxIP);
+            this.GroupConnection.Location = new System.Drawing.Point(664, 341);
+            this.GroupConnection.Name = "GroupConnection";
+            this.GroupConnection.Size = new System.Drawing.Size(133, 107);
+            this.GroupConnection.TabIndex = 3;
+            this.GroupConnection.TabStop = false;
+            this.GroupConnection.Text = "Connection";
+            this.GroupConnection.Enter += new System.EventHandler(this.GroupConnection_Enter);
+            // 
+            // TextBoxPort
+            // 
+            this.TextBoxPort.AccessibleName = "TextBoxPort";
+            this.TextBoxPort.Location = new System.Drawing.Point(40, 54);
+            this.TextBoxPort.Name = "TextBoxPort";
+            this.TextBoxPort.Size = new System.Drawing.Size(87, 20);
+            this.TextBoxPort.TabIndex = 3;
+            this.TextBoxPort.Text = "6000";
+            this.TextBoxPort.TextChanged += new System.EventHandler(this.TextBoxPort_TextChanged);
+            // 
+            // LabelIP
+            // 
+            this.LabelIP.AccessibleName = "LabelIP";
+            this.LabelIP.AutoSize = true;
+            this.LabelIP.Location = new System.Drawing.Point(14, 31);
+            this.LabelIP.Name = "LabelIP";
+            this.LabelIP.Size = new System.Drawing.Size(20, 13);
+            this.LabelIP.TabIndex = 4;
+            this.LabelIP.Text = "IP:";
+            this.LabelIP.Click += new System.EventHandler(this.LabelIP_Click);
+            // 
+            // LabelPort
+            // 
+            this.LabelPort.AccessibleName = "LabelPort";
+            this.LabelPort.AutoSize = true;
+            this.LabelPort.Location = new System.Drawing.Point(5, 57);
+            this.LabelPort.Name = "LabelPort";
+            this.LabelPort.Size = new System.Drawing.Size(29, 13);
+            this.LabelPort.TabIndex = 5;
+            this.LabelPort.Text = "Port:";
+            this.LabelPort.Click += new System.EventHandler(this.LabelPort_Click);
             // 
             // SysBotUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GroupConnection);
             this.Icon = global::RiseHax.WinForms.Properties.Resources.icon;
             this.Name = "SysBotUI";
             this.Text = "RiseHax";
             this.Load += new System.EventHandler(this.SysBotUI_Load);
+            this.GroupConnection.ResumeLayout(false);
+            this.GroupConnection.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TextBoxIP;
+        private System.Windows.Forms.Button ButtonConnect;
+        private System.Windows.Forms.GroupBox GroupConnection;
+        private System.Windows.Forms.Label LabelPort;
+        private System.Windows.Forms.Label LabelIP;
+        private System.Windows.Forms.TextBox TextBoxPort;
     }
 }
