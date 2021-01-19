b574c5f9c7b33bb695698fbaec2357a946b302cf
Glazelf <gideon.steenbergen@gmail.com>
2021-01-19 12:39:07 +0100

    Various settings
    And icons (hopefully)

--- RiseHax.WinForms/Main.Designer.cs
+++ RiseHax.WinForms/Main.Designer.cs
@@ -21,50 +21,48 @@ namespace RiseHax.WinForms
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         /// <summary>
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.ButtonLiveHax = new System.Windows.Forms.Button();
             this.ButtonSaveEdit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ButtonLiveHax
             // 
-            this.ButtonLiveHax.Location = new System.Drawing.Point(75, 20);
+            this.ButtonLiveHax.Location = new System.Drawing.Point(64, 17);
             this.ButtonLiveHax.Name = "ButtonLiveHax";
-            this.ButtonLiveHax.Size = new System.Drawing.Size(175, 60);
+            this.ButtonLiveHax.Size = new System.Drawing.Size(150, 52);
             this.ButtonLiveHax.TabIndex = 0;
             this.ButtonLiveHax.Text = "LiveHax";
             this.ButtonLiveHax.UseVisualStyleBackColor = true;
             // 
             // ButtonSaveEdit
             // 
-            this.ButtonSaveEdit.Location = new System.Drawing.Point(75, 100);
+            this.ButtonSaveEdit.Location = new System.Drawing.Point(64, 87);
             this.ButtonSaveEdit.Name = "ButtonSaveEdit";
-            this.ButtonSaveEdit.Size = new System.Drawing.Size(175, 60);
+            this.ButtonSaveEdit.Size = new System.Drawing.Size(150, 52);
             this.ButtonSaveEdit.TabIndex = 1;
             this.ButtonSaveEdit.Text = "Save Edit";
             this.ButtonSaveEdit.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 191);
+            this.ClientSize = new System.Drawing.Size(286, 166);
             this.Controls.Add(this.ButtonSaveEdit);
             this.Controls.Add(this.ButtonLiveHax);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "RiseHax";
             this.ResumeLayout(false);
 
         }
-
-        #endregion
 
         private System.Windows.Forms.Button ButtonLiveHax;
         private System.Windows.Forms.Button ButtonSaveEdit;
