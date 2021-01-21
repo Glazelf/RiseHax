b92926803235ab22435cd94ed4616670829ba0e0
Glazelf <gideon.steenbergen@gmail.com>
2021-01-21 12:38:02 +0100

    add offset file

---
+++ RiseHax.Hunter/Structures/RAM/DataOffsets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RiseHax.Hunter
+{
+    public static class DataOffsets
+    {
+        public const uint HunterHPOffset = 0x001DFC09C0;
+        // Might just be Pouch slot 2(?)
+        public const uint MegaPotionOffset = 0x001D00C334;
+    }
+}
