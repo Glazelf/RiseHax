17886ec7eec2d44ee71f4dc1d80ad02ee822649f
Glazelf <gideon.steenbergen@gmail.com>
2021-01-22 01:27:55 +0100

    update potion offset
    isnt even static wtf

--- RiseHax.Hunter/Structures/RAM/DataOffsets.cs
+++ RiseHax.Hunter/Structures/RAM/DataOffsets.cs
@@ -5,7 +5,7 @@ namespace RiseHax.Hunter
     public static class DataOffsets
     {
         public const uint HunterHPOffset = 0x001DFC09C0;
-        // Might just be Pouch slot 2(?)
-        public const uint MegaPotionOffset = 0x001D00C334;
+        // Might just be Pouch slot 2(?) (also doesn't seem to be static lol)
+        public const uint MegaPotionOffset = 0x001D00BD54;
     }
 }
