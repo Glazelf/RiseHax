b81c04c48d7534a33f69562c72f264e7ec33974a
Glazelf <gideon.steenbergen@gmail.com>
2021-01-24 01:20:02 +0100

    add Pointer handling

--- RiseHax.Hunter/Structures/RAM/DataOffsets.cs
+++ RiseHax.Hunter/Structures/RAM/DataOffsets.cs
@@ -5,8 +5,10 @@ namespace RiseHax.Hunter
     public static class DataOffsets
     {
         // None of these seem to be static wtf i hate demos
-        public const uint HunterHPOffset = 0x001DFC86FC;
+        public const uint OffsetHunterHP = 0x001DFC86FC;
         // Might just be Pouch slot 2(?)
-        public const uint MegaPotionOffset = 0x001D00BD54;
+        public const uint OffsetMegaPotion = 0x001D00BD54;
+
+        public const string PointerHunterHP = "[[[[main+CD7AB00]+28]+390]+20]+10";
     }
 }
---
+++ RiseHax.WinForms/Util/PointerHandler.cs
@@ -0,0 +1,76 @@
+﻿using RiseHax.Core;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace RiseHax.WinForms
+{
+    class PointerHandler
+    {
+        private static bool IsNum(char c) => (uint)(c - '0') <= 9;
+        private static bool IsHexUpper(char c) => (uint)(c - 'A') <= 5;
+        private static bool IsHexLower(char c) => (uint)(c - 'a') <= 5;
+
+        /// <summary>
+        /// Parses the hex string into a <see cref="uint"/>, skipping all characters except for valid digits.
+        /// </summary>
+        /// <param name="value">Hex String to parse</param>
+        /// <returns>Parsed value</returns>
+        public static uint GetHexValue(string value)
+        {
+            uint result = 0;
+            if (string.IsNullOrEmpty(value))
+                return result;
+
+            foreach (var c in value)
+            {
+                if (IsNum(c))
+                {
+                    result <<= 4;
+                    result += (uint)(c - '0');
+                }
+                else if (IsHexUpper(c))
+                {
+                    result <<= 4;
+                    result += (uint)(c - 'A' + 10);
+                }
+                else if (IsHexLower(c))
+                {
+                    result <<= 4;
+                    result += (uint)(c - 'a' + 10);
+                }
+            }
+            return result;
+        }
+
+        public ulong GetPointerAddress(ISwitchConnectionSync sb, string ptr)
+        {
+            uint finadd = 0;
+            if (!ptr.EndsWith("]"))
+                finadd = GetHexValue(ptr.Split('+').Last());
+            var jumps = ptr.Replace("main", "").Replace("[", "").Replace("]", "").Split(new[] { "+" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var initaddress = GetHexValue(jumps[0].Trim());
+            ulong address = BitConverter.ToUInt64(sb.ReadBytesMain(initaddress, 0x8), 0);
+            foreach (var j in jumps)
+            {
+                var val = GetHexValue(j.Trim());
+                if (val == initaddress)
+                    continue;
+                if (val == finadd)
+                {
+                    address += val;
+                    break;
+                }
+                address = BitConverter.ToUInt64(sb.ReadBytesAbsolute(address + val, 0x8), 0);
+            }
+            return address;
+        }
+
+        public string DecimalToHex(ulong decValue)
+        {
+            string hexValue = "0x" + decValue.ToString("X");
+            return hexValue;
+        }
+    }
+}
