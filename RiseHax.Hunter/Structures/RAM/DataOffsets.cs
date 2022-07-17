d579e5c7584dafd9b1f6b2e705093424da863d7d
Gideon <35176230+Glazelf@users.noreply.github.com>
2022-07-17 23:33:33 +0200

    Updated classification of "outdated" pointers
    It's all of them lol

--- RiseHax.Hunter/Structures/RAM/DataOffsets.cs
+++ RiseHax.Hunter/Structures/RAM/DataOffsets.cs
@@ -5,34 +5,27 @@
         /// <summary>
         /// Pointers
         /// </summary>
-        //////////////////////////// Updated for v3.0:
+        //////////////////////////// Updated pointers (10.0.3):
+        
+        //////////////////////////// Outdated pointers:
         // General
         public const string PointerZeni = "[heap+001DEE5428]"; // Notes: Max 999.999.999?
         public const string PointerPoints = "[heap+1DEDB390]"; // Notes: Max 999.999.999?
-
         // Still need to add code for these
         public const string PointerDangoTickets = "[heap+1D95BC94]"; // Notes: Max 9999?
-
-        //////////////////////////// Outdated pointers:
-
-        /// Quest
+        //// Quest
         // Max HP at +0x
         public const string PointerHunterHP = "[[[[[main+D5F9DF0]+448]+140]+A0]+118]+10";
-
         // Coords directly follow eachother
         public const string PointerHunterCoordX = "[[[[main+CDBBF88]+A0]+18]+30";
         // SHOULD work for all quests with 3 monsters but not widely tested
         public const string PointerMonsters3HP = "[[[main+CD776B0]+50]+280]+170";
-
         // Doubles as Village Quest pointer
         public const string PointerActivityLog = "[[main+D96F848]+70]+80";
-
         public const string PointerPouchItem1 = "[[[[main+D9674B8]+78]+18]+40]+10";
-
         // Add 0x8 to the second to last number to get the next item.
         public const string PointerLayeredArmor = "[[[[[main+0D9674B8]+D8]+10]+10]+20]+20";
         public const string PointerLayeredArmorBuddies = "[[[[[main+0D9674B8]+D0]+28]+10]+20]+10";
-
 
         /// <summary>
         /// Distances
